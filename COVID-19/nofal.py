38d9030fe1cb700a65b99ced1d9854e9242d21d5
mahnofal <manofal@cisco.com>
2021-05-03 10:21:01 +0300

    Create nofal.py

---
+++ COVID-19/nofal.py
@@ -0,0 +1,13 @@
+A = input ("What is your name? ")
+B = input ("How old are you? ")
+print ("")
+print ("****************Cisco COVID-19 Advisor*******************")
+print ("")
+print ("Hello " , A)
+B = float(B)
+if B <= 16:
+   print ("No need for vaccination")
+if B > 16:
+   print ("Get your vaccination ASAP")
+print ("")
+print ("*********************Stay Safe***************************")
